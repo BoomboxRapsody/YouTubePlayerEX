7d36edce8a32b1c6c355cae253a78a19fa2ab0af
BoomboxRapsody <seonghyeon802@gmail.com>
2026-02-12 13:28:29 +0900

    Complete `BackdropBlurContainer`

--- YouTubePlayerEX.App/Graphics/Containers/BackdropBlurContainer.cs
+++ YouTubePlayerEX.App/Graphics/Containers/BackdropBlurContainer.cs
@@ -1,20 +1,23 @@
 ﻿// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable disable
+
+using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Shaders;
+using osu.Framework.Layout;
 using osuTK;
 using osuTK.Graphics;
 
 namespace YouTubePlayerEX.App.Graphics.Containers
 {
     /// <summary>
-    /// A container that blurs the content of its nearest parent <see cref="IBufferedContainer"/> behind its children.
+    /// A container that blurs the content of its nearest parent <see cref="IBackbufferProvider"/> behind its children.
     /// If all children are of a specific non-<see cref="Drawable"/> type, use the
     /// generic version <see cref="BackdropBlurContainer{T}"/>.
     /// </summary>
@@ -25,59 +28,66 @@ namespace YouTubePlayerEX.App.Graphics.Containers
     /// <summary>
     /// A container that blurs the content of its nearest parent <see cref="IBufferedContainer"/> behind its children.
     /// </summary>
-    public partial class BackdropBlurContainer<T> : Container<T>, IBufferedContainer, IBufferedDrawable where T : Drawable
+    public partial class BackdropBlurContainer<T> : Container<T>, IBufferedContainer, IBackdropBlurDrawable where T : Drawable
     {
+        public Vector2 BlurSigma
+        {
+            get => blurSigma;
+            set
+            {
+                if (value == blurSigma)
+                    return;
+
+                blurSigma = value;
+                updateRefCount();
+            }
+        }
+
+        private Vector2 blurSigma;
+
+        public float BlurRotation { get; set; }
+
+        public virtual float BackdropOpacity => 1;
+
+        public float MaskCutoff { get; set; }
+
+        public float BackdropTintStrength { get; set; }
+
+        public Vector2 EffectBufferScale { get; set; } = Vector2.One;
+
         [Resolved]
-        private IBufferedContainer parentBufferedContainer { get; set; } = null!;
-
-        /// <summary>
-        /// Controls the amount of blurring in two orthogonal directions (X and Y if
-        /// <see cref="BlurRotation"/> is zero).
-        /// Blur is parametrized by a gaussian image filter. This property controls
-        /// the standard deviation (sigma) of the gaussian kernel.
-        /// </summary>
-        public Vector2 BlurSigma { get; set; } = Vector2.Zero;
-
-        /// <summary>
-        /// Rotates the blur kernel clockwise. In degrees. Has no effect if
-        /// <see cref="BlurSigma"/> has the same magnitude in both directions.
-        /// </summary>
-        public float BlurRotation;
-
-        /// <summary>
-        /// The multiplicative colour of drawn buffered object after applying all effects (e.g. blur). Default is <see cref="Color4.White"/>.
-        /// </summary>
-        public ColourInfo EffectColour = Color4.White;
-
-        /// <summary>
-        /// The alpha at which the content is no longer considered opaque & the background will not be blurred behind it.
-        /// </summary>
-        public float MaskCutoff;
+        private IBackbufferProvider backbufferProvider { get; set; } = null!;
 
         public IShader TextureShader { get; private set; } = null!;
 
+        private readonly BackdropBlurDrawNodeSharedData sharedData;
+
+        public BackdropBlurContainer(RenderBufferFormat[] formats = null)
+        {
+            sharedData = new BackdropBlurDrawNodeSharedData(formats);
+        }
+
+        IShader IBackdropBlurDrawable.BlurShader => blurShader;
+
+        IShader IBackdropBlurDrawable.BlendShader => backdropBlurShader;
+
         private IShader blurShader = null!;
 
-        private IShader textureMaskShader = null!;
-
-        private readonly BackdropBlurContainerDrawNodeSharedData sharedData;
-
-        public BackdropBlurContainer(RenderBufferFormat[]? formats = null, bool pixelSnapping = false)
-        {
-            sharedData = new BackdropBlurContainerDrawNodeSharedData(formats, pixelSnapping);
-        }
+        private IShader backdropBlurShader = null!;
 
         [BackgroundDependencyLoader]
         private void load(ShaderManager shaders)
         {
             TextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE);
             blurShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.BLUR);
-            textureMaskShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE_MASK);
+            backdropBlurShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.BACKDROP_BLUR_BLEND);
         }
 
-        protected override DrawNode CreateDrawNode() => new BackdropBlurDrawNode(this, sharedData);
+        protected override DrawNode CreateDrawNode() => new BackdropBlurContainerDrawNode(this, new CompositeDrawableDrawNode(this), sharedData);
 
-        private RectangleF lastParentDrawRect;
+        private RectangleF lastBackBufferDrawRect;
+
+        RectangleF IBackdropBlurDrawable.LastBackBufferDrawRect => lastBackBufferDrawRect;
 
         protected override void Update()
         {
@@ -85,7 +95,35 @@ namespace YouTubePlayerEX.App.Graphics.Containers
 
             Invalidate(Invalidation.DrawNode);
 
-            lastParentDrawRect = parentBufferedContainer.ScreenSpaceDrawQuad.AABBFloat;
+            lastBackBufferDrawRect = backbufferProvider.ScreenSpaceDrawQuad.AABBFloat;
+        }
+
+        private bool isRefCounted;
+
+        protected override bool OnInvalidate(Invalidation invalidation, InvalidationSource source)
+        {
+            if ((invalidation & Invalidation.Parent) > 0)
+                updateRefCount();
+
+            return base.OnInvalidate(invalidation, source);
+        }
+
+        private void updateRefCount()
+        {
+            if (backbufferProvider is RefCountedBackbufferProvider refCount)
+            {
+                bool shouldBeRefCounted = Parent != null && (BlurSigma.X > 0 || BlurSigma.Y > 0);
+
+                if (shouldBeRefCounted != isRefCounted)
+                {
+                    if (shouldBeRefCounted)
+                        refCount.Increment();
+                    else
+                        refCount.Decrement();
+
+                    isRefCounted = shouldBeRefCounted;
+                }
+            }
         }
 
         public Color4 BackgroundColour => Color4.Transparent;
@@ -106,8 +144,35 @@ namespace YouTubePlayerEX.App.Graphics.Containers
 
         public Vector2 FrameBufferScale { get; set; } = Vector2.One;
 
-        public Vector2 EffectBufferScale { get; set; } = Vector2.One;
-
         public float GrayscaleStrength { get; set; } = 0;
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isRefCounted && backbufferProvider is RefCountedBackbufferProvider refCount)
+            {
+                refCount.Decrement();
+                isRefCounted = false;
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+        private class BackdropBlurContainerDrawNode : BackdropBlurDrawNode, ICompositeDrawNode
+        {
+            public BackdropBlurContainerDrawNode(IBufferedDrawable source, CompositeDrawableDrawNode child, BackdropBlurDrawNodeSharedData sharedData)
+                : base(source, child, sharedData)
+            {
+            }
+
+            protected new CompositeDrawableDrawNode Child => (CompositeDrawableDrawNode)base.Child;
+
+            public List<DrawNode> Children
+            {
+                get => Child.Children;
+                set => Child.Children = value;
+            }
+
+            public bool AddChildDrawNodes => RequiresRedraw;
+        }
     }
 }
