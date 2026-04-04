8bb07fa080615ede181b094a5c98786b3469c731
BoomboxRapsody <seonghyeon802@gmail.com>
2026-04-04 09:24:08 +0900

    Update resources

--- NekoPlayer.App/Localisation/NekoPlayerStrings.cs
+++ NekoPlayer.App/Localisation/NekoPlayerStrings.cs
@@ -467,9 +467,9 @@ namespace NekoPlayer.App.Localisation
         public static LocalisableString CommentAdded => new TranslatableString(getKey(@"comment_added"), "Comment added.");
 
         /// <summary>
-        /// "Write comment as {0}"
+        /// "Write comment as {0}..."
         /// </summary>
-        public static LocalisableString CommentWith(string username) => new TranslatableString(getKey(@"comment_with"), "Write comment as {0}", username);
+        public static LocalisableString CommentWith(string username) => new TranslatableString(getKey(@"comment_with"), "Write comment as {0}...", username);
 
         /// <summary>
         /// "Signed in to {0}"
